878f83e3d1bc85286a8d564c8996c39dbe13099b
categoryK <categoryK@DESKTOP-8Q6NURQ>
2021-02-02 12:25:43 +0900

    스프링5용 헤로쿠배포

--- src/main/java/org/edu/service/MemberServiceImpl.java
+++ src/main/java/org/edu/service/MemberServiceImpl.java
@@ -62,7 +62,7 @@ public class MemberServiceImpl implements IF_MemberService {
 	@Override
 	public void herokuJobMethod() throws Exception {
 		// 월-금 오전8시부터 오후11시 (미국시간 23,0-14) 헤로쿠앱에 20분 간격으로 URL접근
-		String urlStr ="https://kimbeomjoo.herokuapp.com";//본인앱주소
+		String urlStr ="https://spring5-kimbeomjoo.herokuapp.com";//본인앱주소
 		URL url = new URL(urlStr);
 		HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();//접속시작
 		urlConnection.setUseCaches(false);//기존접속된 정보가 있든지 없든지 무조건 접속
