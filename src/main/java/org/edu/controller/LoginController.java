c7ea389c000ed7ec4c7acc15cb7deefb50ab7eec
categoryK <categoryK@DESKTOP-8Q6NURQ>
2021-02-02 16:22:00 +0900

    네아로 로그인 취소 오류처리후 헤로쿠 배포

--- src/main/java/org/edu/controller/LoginController.java
+++ src/main/java/org/edu/controller/LoginController.java
@@ -42,8 +42,12 @@ public class LoginController {
 	//로그인 세션 처리 매핑 -네이버 로그인 아이디 로직일때
 	//session(인증토큰정보), state(유효성검증용 UUID정보),code(인증성공/실패 코드 예, 00 Ok / 401 Error)
 	@RequestMapping(value="/login_callback", method={RequestMethod.GET, RequestMethod.POST})
-	public String login_callback(Model model, @RequestParam String code, @RequestParam String state, HttpSession session, RedirectAttributes rdat) throws IOException, ParseException {
-		
+	public String login_callback(Model model, @RequestParam(required=false) String code, @RequestParam String state, HttpSession session, RedirectAttributes rdat) throws IOException, ParseException {
+		//네아로에서 로그인 취소 했을때 code값이 null일때 처리(아래)
+		if(code ==null) {
+			rdat.addFlashAttribute("naver_msg", "fail");
+			return "redirect:/login";
+		}
 		OAuth2AccessToken oauthToken;//토큰으로 사용할 변수 선언
 		//NaverLoginController의 메서드 호출(아래)
 		oauthToken = naverLoginController.getAccessToken(session, code, state);//인증결과
